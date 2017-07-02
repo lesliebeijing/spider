91a7e6adc7fa9d8e55c893d49980dc89266de774
fanglin <fanglin@chinayouthgroup.com>
2017-07-02 12:35:19 +0800

    update

--- HelloScrapy/spiders/douban.py
+++ HelloScrapy/spiders/douban.py
@@ -1,6 +1,7 @@
 import re
 
 import scrapy
+import time
 
 """
 豆瓣搜索图书并爬取列表信息
@@ -14,7 +15,7 @@ class DoubanSpider(scrapy.Spider):
 
     def start_requests(self):
         for key in self.search_keys:
-            for i in range(0, 1):
+            for i in range(0, 100):
                 start = i * self.page_size
                 url = 'https://book.douban.com/subject_search?start=%s&search_text=%s&cat=1001' % (start, key)
                 yield scrapy.Request(url=url, callback=self.parse)
@@ -24,9 +25,11 @@ class DoubanSpider(scrapy.Spider):
 
         for li in lis:
             score = li.xpath('div[@class="info"]/div[2]/span[2]/text()').extract_first()
-            comments = li.xpath('div[@class="info"]/div[2]/span[3]/text()').extract_first()
             if score and float(score) >= 8.0:
+                img_url = li.xpath('div[@class="pic"]/a/img/@src').extract_first()
+                comments = li.xpath('div[@class="info"]/div[2]/span[3]/text()').extract_first()
                 title = li.xpath('div[@class="info"]/h2/a/@title').extract_first()
+                url = li.xpath('div[@class="info"]/h2/a/@href').extract_first()
                 pub = li.xpath('div[@class="info"]/div[@class="pub"]/text()').extract_first()
                 press = ''
                 press_year = ''
@@ -43,20 +46,30 @@ class DoubanSpider(scrapy.Spider):
                         press = press_arr[2]
                         press_year = press_arr[3]
 
+                if press_year:
+                    tmp = re.split(r'-', press_year)
+                    year = int(tmp[0])
+                    cur_year = time.localtime(time.time()).tm_year
+                    if cur_year - year > 10:
+                        # 只看 10 年以内的
+                        continue
+
                 comments_count = 0
                 if comments:
                     comments = comments.strip()
                     cnt = re.findall(r'\d+', comments)
                     if cnt:
-                        comments_count = cnt[0]
+                        comments_count = int(cnt[0])
 
-                print(title, author, press, press_year, score, comments_count)
+                print(title, img_url, author, press, press_year, score, comments_count)
 
                 yield {
                     'name': title,
+                    'img_url': img_url,
+                    'url': url,
                     'author': author,
                     'press': press,
                     'press_year': press_year,
                     'score': float(score),
-                    'comments_count': float(comments_count)
+                    'comments_count': comments_count
                 }
